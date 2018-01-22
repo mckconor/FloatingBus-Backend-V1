2cf5d21ef942d65c4426eb99a92bf3e1b65bed88
Conor McKenna <mckennc9@tcd.ie>
2018-01-22 02:29:49 +0000

    Initial Commit
    Very basic impl, needs more work with incoming data

---
+++ src/main/java/Components/Coordinates.java
@@ -0,0 +1,12 @@
+package Components;
+
+public class Coordinates {
+	
+	public double longitude, latitude;
+	
+	public Coordinates (double d, double e) {
+		this.longitude = d;
+		this.latitude = e;
+	}
+	
+}
